cb8e6c6964153816e05295870c6d467e7d082f4c
luixfds <l.fds@yahoo.com>
2021-07-12 10:16:05 -0300

    Cadastro cliente

--- cliente/urls.py
+++ cliente/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 from home.views import index
 urlpatterns = [
-    path('listaClientes/', views.listaCliente, name='clientes'),
+    path('cadClientes/', views.listaCliente, name='clientes'),
     path('', index, name='home'),
 ]
