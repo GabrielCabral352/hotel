056682bd7816f718e0c2f9ec69258984584d5c75
GabrielCabral352 <gabrielcabral352@gmail.com>
2021-07-16 11:11:37 -0300

    client delete

--- cliente/urls.py
+++ cliente/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('cadClientes/', views.listaCliente, name='clientes'),
     path('', views.cadCliente, name='inserir'),
     path('listagem/', views.listCliente, name='lista_cliente'),
+    path('delCliente/?P<int:id>', views.clienteDelete, name="delCliente"),
     path('', index, name='home'),
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -9,17 +9,20 @@ def listaCliente(request):
 
 
 def cadCliente(request):
-    email = request.POST['email']
-    name = request.POST['name']
-    cpf = request.POST['cpf']
-    password = request.POST['password']
+    try:
+        email = request.POST['email']
+        name = request.POST['name']
+        cpf = request.POST['cpf']
+        password = request.POST['password']
 
-    with conn.cursor() as insere:
-        sql = "insert into tb_clientes(name,email,cpf,senha) values (%s, %s, %s, %s)"
-        insere.execute(sql, (email, name, cpf, password))
-        insere.close()
-        conn.commit()
-    return render(request, 'cliente/index.html')
+        with conn.cursor() as insere:
+            sql = "insert into tb_clientes(name,email,cpf,senha) values (%s, %s, %s, %s)"
+            insere.execute(sql, (name, email, cpf, password))
+            insere.close()
+            conn.commit()
+        return render(request, 'cliente/index.html')
+    except:
+        pass
 
 
 def listCliente(request):
@@ -27,5 +30,15 @@ def listCliente(request):
         sql = "select * from tb_clientes"
         read.execute(sql)
         dados = read.fetchall()
-    print(dados)
+        read.close()
     return render(request, 'cliente/load_clientes.html', {'dados': dados})
+
+
+def clienteDelete(request, id):
+    with conn.cursor() as delete:
+        sql = "delete from tb_clientes where id = %s"
+        delete.execute(sql, id)
+        conn.commit()
+        delete.close()
+    return render(request, 'cliente/load_clientes.html')
+
