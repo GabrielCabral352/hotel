8e373c354e8adce74f7c41e75bf3ee1461b9ce98
GabrielCabral352 <gabrielcabral352@gmail.com>
2021-07-19 11:15:05 -0300

    init reservas

--- reservas/urls.py
+++ reservas/urls.py
@@ -4,4 +4,6 @@ from home.views import index
 
 urlpatterns = [
     path('cadReserva/', views.adicionaReserva, name='reserva'),
+    path('', views.inserirReserva, name='fazer_reserva'),
+    path('listaReserva/', views.listaReserva, name='lista_reservas'),
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
-
+from static.hotel.bd.connection import conn
 # Create your views here.
 
 
 def adicionaReserva(request):
-    return render(request, 'reservas/cadReservas.html')
+    with conn.cursor() as read:
+        sql = "select * from tb_clientes"
+        read.execute(sql)
+        dados = read.fetchall()
+        read.close()
+
+    with conn.cursor() as read:
+        sql = "select * from tb_suites where sit = 0"
+        read.execute(sql)
+        dados2 = read.fetchall()
+        read.close()
+    return render(request, 'reservas/cadReservas.html', {'dados': dados, 'dados2': dados2})
 
 
 def listaReserva(request):
+    return render(request, 'reservas/listaReservas.html')
+
+
+def inserirReserva(request):
+    cliente = request.POST['cliente']
+    data = request.POST['data']
+    data2 = request.POST['data2']
+    quarto = request.POST['quarto']
+    with conn.cursor() as insere:
+        sql = "insert into tb_reservas(cliente,data_ini,data_end,quarto) values (%s, %s, %s, %s)"
+        insere.execute(sql, (cliente, data, data2, quarto))
+        insere.close()
+        conn.commit()
     return render(request, 'reservas/listaReservas.html')
